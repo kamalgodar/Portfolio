528955cab09b4feea5b23f320408873306699a3d
kamalgodar <kamalgodar9244@gmail.com>
2020-07-07 05:33:59 -1200

    commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// for the on blur event to loose focus on the collapsable navbar
+
+// $(function () {
+//     $(".navbar-toggler").blur(function (event) {
+//         var screenWidth = window.innerWidth;
+//         if (screenWidth < 768) {
+//             $("#navbarSupportedContent").collapse('hide');
+//         }
+//     });
+// });
+
+
+
+// for handling the active status of the page we are currently in mainly if it is a single page website
+
+
+// $("a").click(function(){
+//     $("a").css("background-color","");
+//     $(this).css("background-color","green");
+// });
